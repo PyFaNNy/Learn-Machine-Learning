0415a1c5ebecf2dd497d9f672bb2871b2b7209b6
Mr.Bubble <puzyrev.artyom@gmail.com>
2021-03-22 19:45:23 +0300

    реализация описания

---
+++ C#/NeuralNetworks/Topology.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace NeuralNetworks
+{
+    public class Topology
+    {
+        public int InputCount { get; }
+        public int OutputCount { get; }
+        public List<int> HiddenLayers { get; }
+
+        public Topology(int inputCount, int outputCount, params int[] layers)
+        {
+            //TODO: Проверить входные значения
+            InputCount = inputCount;
+            OutputCount = outputCount;
+            HiddenLayers = new List<int>();
+            HiddenLayers.AddRange(layers);
+        }
+    }
+}
